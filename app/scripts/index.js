4c1d0620de8178416ae94922903a952124cce896
David Nine <pairof9s@gmail.com>
2016-06-08 18:54:17 -0400

    afternoon edits

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,13 @@
+var $ = require('jquery');
+var handlebars = require('Handlebars');
+var _ = require('underscore');
+
 console.log("Hello World!");
+
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=yarn&includes=Images,Shop&sort_on=score";
+var totalItems = url.length;
+
+var source = $('#header-template').html();
+var template = handlebars.compile(source);
+var context = {"count": totalItems};
+var renderTemplate = template(context);
